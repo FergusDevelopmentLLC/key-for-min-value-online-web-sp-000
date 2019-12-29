be4777a119a2f0a66712e03b02898a5b8b380112
Will Carter <will.carter@fergusllc.com>
2019-12-29 22:43:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
-
 def key_for_min_value(name_hash)
   name_hash.collect do |name, value|
       age
   end
 end
+
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+puts key_for_min_value(veggies)
