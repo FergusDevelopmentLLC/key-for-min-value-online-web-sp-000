92d7b4d520bcccb93b9e8e47cb805ff539136dc0
Will Carter <will.carter@fergusllc.com>
2019-12-29 22:45:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
+  value = hash.values[0]
+  puts value
   name_hash.collect do |name, value|
       value
   end
