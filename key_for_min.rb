429776b024e2fe45e21abee9737624362113e755
Will Carter <will.carter@fergusllc.com>
2019-12-29 22:55:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,5 +12,5 @@ def key_for_min_value(name_hash)
   desired_key
 end
 
-veggies = {"apple" => -45, "banana" => -74.5, "carrot" => -64.9}
+veggies = {"apple" => -45, "banana" => -74.5, "carrot" => -84.9}
 puts key_for_min_value(veggies)
